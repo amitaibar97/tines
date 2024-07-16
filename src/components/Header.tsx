da0085e176890865fad159002284f0e7ecdac5fa
amitai.bar <amitaibar97>
2024-07-17 00:46:17 +0300

    add arows

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import Menu from "./Menu";
 import Search from "./Search";
 import TinesLogo from "./TinesLogo";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 
 export default function Header() {
   return (
@@ -8,11 +10,27 @@ export default function Header() {
       <div className="flex items-center space-x-2">
         <TinesLogo />
       </div>
-      <nav className="hidden sm:flex text-xs space-x-1.5 md:ml-16">
-        <div className="bg-[#f1eee9] p-2 space-x-3 rounded-lg md:space-x-6 lg:space-x-10 md:min-w-[17rem] lg:min-w-[21rem]">
-          <button className="font-medium">Product</button>
-          <button className="font-medium">Solutions</button>
-          <button className="font-medium">Resources</button>
+      <nav className="hidden sm:flex text-xs space-x-1.5 md:ml-16 lg:text-[0.813rem]">
+        
+        <div className="bg-[#f1eee9] p-2 space-x-3 rounded-lg md:space-x-6 md:min-w-[17rem] lg:min-w-[21rem] flex items-center lg:px-4">
+          <button className="font-medium flex items-center">
+            Product
+            <span className="hidden lg:inline-flex pl-1">
+              <ExpandMoreIcon fontSize="small" />
+            </span>
+          </button>
+          <button className="font-medium flex items-center">
+            Solutions
+            <span className="hidden lg:inline-flex pl-1">
+              <ExpandMoreIcon fontSize="small" />
+            </span>
+          </button>
+          <button className="font-medium flex items-center">
+            Resources
+            <span className="hidden lg:inline-flex pl-1">
+              <ExpandMoreIcon fontSize="small" />
+            </span>
+          </button>
           <button className="font-medium">Pricing</button>
         </div>
         <button className="bg-[#f1eee9] px-2.5 rounded-full">
@@ -20,12 +38,12 @@ export default function Header() {
         </button>
       </nav>
       <div className="flex items-center space-x-2 text-xs justify-center ">
-        <button className="hidden sm:block font-medium">Log in</button>
+        <button className="hidden sm:block font-medium hover:bg-[#8d75e62e] focus:bg-[#8d75e62e] px-3 py-2 rounded-md">Log in</button>
         <div className=" text-slate-400 hidden sm:block text-base font-light">
           |
         </div>
-        <button className="hidden sm:block font-medium">Sign up</button>
-        <button className="hidden sm:block px-3 py-2 bg-[#8d75e6] text-white rounded-md text-[0.813rem] font-semibold">
+        <button className="hidden sm:block font-medium hover:bg-[#8d75e62e] focus:bg-[#8d75e62e] px-3 py-2 rounded-md">Sign up</button>
+        <button className="hidden sm:block px-3 py-2 bg-[#8d75e6] text-white rounded-md text-[0.813rem] font-semibold hover:bg-[#8d75e6b3] focus:bg-[#8d75e6b3]">
           Book a demo
         </button>
         <div className="sm:hidden flex items-center space-x-6 justify-center">
