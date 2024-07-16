9536ee52c79b1b445f1751ebb412fe5ef5cdc474
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-07-16 23:48:04 +0300

    svg icons

---
+++ src/components/Menu.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <svg
+      width="18"
+      height="17"
+      viewBox="0 0 18 17"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    >
+      <path d="M3 3.5H15"></path>
+      <path d="M3 8.5H15"></path>
+      <path d="M3 13.5H15"></path>
+    </svg>
+  );
+}
